9f4066eb2c7febd6fd0a31f1af0fa1cffc1204b6
Slavek592 <71459428+slavek592@users.noreply.github.com>
2023-11-30 19:57:40 +0100

    New GameSettings

--- BattleShipEngine/GameSetting.cs
+++ BattleShipEngine/GameSetting.cs
@@ -11,6 +11,17 @@ public readonly record struct GameSetting(
     int Width,
     int[] BoatCount)
 {
+    public static bool AreSame(GameSetting s1, GameSetting s2)
+    {
+        if (s1.Width != s2.Width || s1.Height != s2.Height
+                                 || s1.BoatCount.Length != s2.BoatCount.Length)
+            return false;
+        for (int i = 0; i < s1.BoatCount.Length; i++)
+            if (s1.BoatCount[i] != s2.BoatCount[i])
+                return false;
+        return true;
+    }
+    
     /// <summary>
     /// Settings that can be found here <see href="https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7"></see>
     /// </summary>
@@ -18,4 +29,19 @@ public readonly record struct GameSetting(
         10, 
         10, 
         new[] {4, 3, 2, 1});
+    
+    public static GameSetting Hasbro = new(
+        10, 
+        10, 
+        new[] {0, 1, 2, 1, 1});
+    
+    public static GameSetting Large = new(
+        12, 
+        12, 
+        new[] {0, 3, 2, 2, 1});
+    
+    public static GameSetting Small = new(
+        5, 
+        5, 
+        new[] {2, 1, 1});
 }
