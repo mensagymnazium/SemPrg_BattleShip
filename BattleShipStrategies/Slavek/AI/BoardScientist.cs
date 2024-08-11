89124af4b9dcff4f01a1bf0eebb94511a4eded42
Slavek592 <nikiforov.vjaceslav@gmail.com>
2024-08-11 17:36:16 +0200

    New AI tools

--- BattleShipStrategies/Slavek/AI/BoardScientist.cs
+++ BattleShipStrategies/Slavek/AI/BoardScientist.cs
@@ -5,7 +5,7 @@ namespace BattleShipStrategies.Slavek.AI;
 
 public static class BoardScientist
 {
-    public static PreparedMap StableNemesis(GameSetting setting, IGameStrategy strategy)
+    public static PreparedMap StableNemesis(GameSetting setting, IGameStrategy strategy, uint tries=10000)
     {
         IBoardCreationStrategy boardStrategy = new SmartRandomBoardCreationStrategy();
         BoatPaster paster = new BoatPaster();
@@ -15,7 +15,7 @@ public static class BoardScientist
         
         Int2[] bestBoats = new Int2[boatTileCount];
         int bestTurns = 0;
-        for (int i = 0; i < 50000; i++)
+        for (uint i = 0; i < tries; i++)
         {
             Int2[] newBoats = boardStrategy.GetBoatPositions(setting);
             paster.SetBoats(newBoats);
@@ -28,14 +28,40 @@ public static class BoardScientist
             }
         }
         Console.WriteLine(bestTurns);
-        /*Game tgame = new Game(paster, setting);
-        for (int i = 0; i < 10; i++)
-            Console.WriteLine(tgame.SimulateGame(strategy));*/
+        return new PreparedMap(setting, bestBoats);
+    }
+    
+    public static PreparedMap UnstableNemesis(GameSetting setting, IGameStrategy strategy,
+        uint tries=10000, uint precision=20)
+    {
+        IBoardCreationStrategy boardStrategy = new SmartRandomBoardCreationStrategy();
+        BoatPaster paster = new BoatPaster();
+        int boatTileCount = 0;
+        for (int i = 0; i < setting.BoatCount.Length; i++)
+            boatTileCount += setting.BoatCount[i] * (i + 1);
+        
+        Int2[] bestBoats = new Int2[boatTileCount];
+        int bestTurns = 0;
+        for (uint i = 0; i < tries; i++)
+        {
+            Int2[] newBoats = boardStrategy.GetBoatPositions(setting);
+            paster.SetBoats(newBoats);
+            Game game = new Game(paster, setting);
+            int newTurns = 0;
+            for (uint j = 0; j < precision; j++)
+                newTurns += game.SimulateGame(strategy);
+            if (newTurns > bestTurns)
+            {
+                bestTurns = newTurns;
+                bestBoats = newBoats;
+            }
+        }
+        Console.WriteLine(bestTurns/precision);
         return new PreparedMap(setting, bestBoats);
     }
 
     public static PreparedMap StableSmart(GameSetting setting, Dictionary<IGameStrategy, double> enemies,
-        Dictionary<IGameStrategy, double> allies)
+        Dictionary<IGameStrategy, double> allies, uint tries=10000)
     {
         IBoardCreationStrategy boardStrategy = new SmartRandomBoardCreationStrategy();
         BoatPaster paster = new BoatPaster();
@@ -45,7 +71,7 @@ public static class BoardScientist
         
         Int2[] bestBoats = new Int2[boatTileCount];
         double bestScore = 0;
-        for (int i = 0; i < 50000; i++)
+        for (uint i = 0; i < tries; i++)
         {
             Int2[] newBoats = boardStrategy.GetBoatPositions(setting);
             paster.SetBoats(newBoats);
@@ -55,6 +81,42 @@ public static class BoardScientist
                 newScore += game.SimulateGame(strategy) * enemies[strategy];
             foreach (IGameStrategy strategy in allies.Keys)
                 newScore -= game.SimulateGame(strategy) * allies[strategy];
+            if (newScore > bestScore)
+            {
+                bestScore = newScore;
+                bestBoats = newBoats;
+            }
+        }
+        Console.WriteLine(bestScore);
+        return new PreparedMap(setting, bestBoats);
+    }
+    
+    public static PreparedMap UnstableSmart(GameSetting setting, Dictionary<IGameStrategy, double> enemies,
+        Dictionary<IGameStrategy, double> allies, uint tries=10000, uint precision=20)
+    {
+        IBoardCreationStrategy boardStrategy = new SmartRandomBoardCreationStrategy();
+        BoatPaster paster = new BoatPaster();
+        int boatTileCount = 0;
+        for (int i = 0; i < setting.BoatCount.Length; i++)
+            boatTileCount += setting.BoatCount[i] * (i + 1);
+        
+        Int2[] bestBoats = new Int2[boatTileCount];
+        double bestScore = 0;
+        for (uint i = 0; i < tries; i++)
+        {
+            Int2[] newBoats = boardStrategy.GetBoatPositions(setting);
+            paster.SetBoats(newBoats);
+            Game game = new Game(paster, setting);
+            double newScore = 0;
+            
+            for (uint j = 0; j < precision; j++)
+            {
+                foreach (IGameStrategy strategy in enemies.Keys)
+                    newScore += game.SimulateGame(strategy) * enemies[strategy];
+                foreach (IGameStrategy strategy in allies.Keys)
+                    newScore -= game.SimulateGame(strategy) * allies[strategy];
+            }
+
             if (newScore > bestScore)
             {
                 bestScore = newScore;
